553d78342c69c4a2955f014854692073af208284
shubhamkunwar10 <fonders@metakul.com>
2025-04-14 19:28:06 +0530

    add text

--- src/Helpers/VideoProcessing.ts
+++ src/Helpers/VideoProcessing.ts
@@ -80,6 +80,7 @@ const createSegment = (
       ])
       .format('mp4')
       .outputOptions([
+        `-vf`, `drawtext=text='${text}':fontcolor=white:fontsize=80:box=1:boxcolor=black@0.5:x=(w-text_w)/2:y=50`,
         '-b:a', '64k',
       ]);
   
